0d999e8817ab39ab478db52c3d77139cdbefafcb
soheil <soheilrasekh555@gmail.com>
2022-01-31 14:52:30 +0330

    add Matrial UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../src/createEmotionCache";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../src/theme";
+import CssBaseline from "@mui/material/CssBaseline";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width"
+        ></meta>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
